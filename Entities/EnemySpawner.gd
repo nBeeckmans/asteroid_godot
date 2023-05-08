cf44e16a5faf6a5bfe54d80388ca73f1a453d510
nBeeckmans <nicolas.beeckmans@hotmail.com>
2023-05-08 13:10:07 -0400

    first commit

--- Entities/EnemySpawner.gd
+++ Entities/EnemySpawner.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+@export var Asteroid : EnemyScene
+
+func _on_spawn_timer_timeout():
+	var A = Asteroid.instantiate() 
+	var mobSpawnLocation = get_node("EnemyPath/EnemySpawnLocation")
+	mobSpawnLocation.progress_ratio = randf() 
+	var direction = mobSpawnLocation.rotation + PI/2
+	A.position = mobSpawnLocation.position 
+	direction += randf_range(-PI/4, PI/4)
+	A.rotation = direction 
+	
+	add_child(A)
